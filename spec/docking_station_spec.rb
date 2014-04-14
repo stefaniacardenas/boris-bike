9e058da675f9fe94008bd5187c13031fece02854
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-04-14 18:15:54 +0100

    Added a docking station

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,11 @@
+require "docking_station"
+
+describe "DockingStation" do 
+	it "should accept a bike" do
+		bike =Bike.new
+		station = DockingStation.new
+		expect(station.bike_count).to eq 0
+		station.dock(bike)
+		expect(station.bike_count).to eq 1
+	end
+end
