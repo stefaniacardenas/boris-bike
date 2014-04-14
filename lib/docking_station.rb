fb42357fa0bef48fa40aff78a489785b109cdae4
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-04-14 19:25:15 +0100

    Added capacity to the station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,9 @@
 class DockingStation
+	DEFAULT_CAPACITY = 10
 
-	def initialize
-	@bikes = []
+	def initialize(options = {})
+		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
+		@bikes = []
 	end
 
 	def bike_count
@@ -14,5 +16,9 @@ class DockingStation
 
 	def release(bike)
 		@bikes.delete(bike)
+	end
+
+	def full?
+		bike_count == @capacity
 	end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@ require "docking_station"
 
 describe "DockingStation" do 
 	let(:bike) { Bike.new }
-	let(:station) { DockingStation.new }
+	let(:station) { DockingStation.new(:capacity => 20) }
 	
 	it "should accept a bike" do
 		expect(station.bike_count).to eq 0
@@ -14,6 +14,12 @@ describe "DockingStation" do
 		station.dock(bike)
 		station.release(bike)
 		expect(station.bike_count).to eq 0
+	end
+
+	it "should know when it's full" do
+		expect(station).not_to be_full
+		20.times { station.dock(Bike.new) }
+		expect(station).to be_full
 	end
 
 end
