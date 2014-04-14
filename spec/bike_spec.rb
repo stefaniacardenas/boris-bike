4710e614e371db3925375c762a3b313fe8d7b7cb
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-04-14 15:53:59 +0100

    Delete a directory and rewritten the test

---
+++ spec/bike_spec.rb
@@ -0,0 +1,13 @@
+require "bike"
+
+describe "Bike" do # this describes the functionality of a specific class (Bike)
+	 it "should not be broken after we create it" do # this is the behaviour that we expect from our test
+	 bike = Bike.new # initialise a new object of the Bike class
+	 expect(bike).not_to be_broken # It exoect that an instance of this class has got a method called broken? and it will return to false
+	 end	
+
+ #it "knows that a bike can break" do
+ 	#expect(Bike.new.brake?).to be_true
+ #end
+
+end
