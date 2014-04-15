490e608d772db1b8090de4c770b2b8447e23b875
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-04-15 15:32:04 +0100

    Refactoring the code using module and composition

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,30 +1,12 @@
+require "./lib/bike_container"
+
 class DockingStation
-	DEFAULT_CAPACITY = 20
+
+	include BikeContainer
 
 	def initialize(options = {})
-		@capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
+		self.capacity = options.fetch(:capacity, capacity)
 		@bikes = []
-	end
-
-	def bike_count
-		@bikes.count
-	end
-
-	def dock(bike)
-		raise "Station is full" if full?
-		@bikes << bike
-	end
-
-	def release(bike)
-		@bikes.delete(bike)
-	end
-
-	def full?
-		bike_count == @capacity
-	end
-
-	def available_bikes
-		@bikes.reject { |bike| bike.broken? }
 	end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,42 +1,10 @@
 require "docking_station"
 
 describe "DockingStation" do 
-	let(:bike) { Bike.new }
-	let(:station) { DockingStation.new(:capacity => 20) }
+
+	let(:station) { DockingStation.new(:capacity => 123) }
 	
-	def fill_station(station)
-		20.times { station.dock(Bike.new) }
+	it "should allow setting default capacity on initialising" do
+		expect(station.capacity).to eq 123
 	end
-	
-	it "should accept a bike" do
-		expect(station.bike_count).to eq 0
-		station.dock(bike)
-		expect(station.bike_count).to eq 1
-	end
-
-	it "should release a bike" do
-		station.dock(bike)
-		station.release(bike)
-		expect(station.bike_count).to eq 0
-	end
-
-	it "should know when it's full" do
-		expect(station).not_to be_full
-		fill_station station
-		expect(station).to be_full
-	end
-
-	it "should not accept a bike if it's full" do
-		fill_station station
-		expect(lambda { station.dock(bike)}).to raise_error(RuntimeError)
-	end
-
-	it "should provide the list of available bikes" do
-		working_bike, broken_bike = Bike.new, Bike.new
-		broken_bike.break
-		station.dock(working_bike)
-		station.dock(broken_bike)
-		expect(station.available_bikes).to eq ([working_bike])
-	end
-
 end
