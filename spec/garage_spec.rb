6c0cf04f32d933a31c9c59793e1a6ff90eda235d
stefaniacardenas <stefaniaferullo@hotmail.com>
2014-07-22 12:08:43 +0100

    Added bike to the garage

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -4,13 +4,15 @@ require "bike"
 describe "Garage" do 
 
 	let(:garage) { Garage.new(:capacity => 123) }
+	let(:bike) { Bike.new }
 	
 	it "should allow setting default capacity on initialising" do
 		expect(garage.capacity).to eq 123
 	end
 
 	it "should fix a bike after it arrives" do
-		expect(bike).not_to be_broken # I'm creating an event for every step just to have a visual understanding of what's going on
+		# I'm creating an event for every step just to have a visual understanding of what's going on
+		expect(bike).not_to be_broken
 		bike.break
 		expect(bike).to be_broken
 		garage.accept(bike)
